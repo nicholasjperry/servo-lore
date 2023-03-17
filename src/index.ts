042cd4f24a8fa1c62efb8ee0d48438036892ee6b
Nick <nicholasjperry1@gmail.com>
2023-03-17 01:38:46 -0400

    Changed PUPPETEER_EXECUTABLE_PATH

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const scrapeData = async () => {
     const browser = await puppeteer.launch({
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        // executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
     });
     const url = 'https://wh40k.lexicanum.com/wiki/Special:Random';
     
@@ -66,7 +66,7 @@ client.on('ready', async () => {
     setInterval(async () => {
         await deleteEmbedMessage();
         await sendEmbedMessage();
-    }, 20000)
+    }, 10000)
 });
 
 // Logging the bot in to the server with token
