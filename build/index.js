768579a0180b0cc9d571c8167f544c4831727355
Nick <nicholasjperry1@gmail.com>
2023-03-17 19:10:48 -0400

    Commenting out executable path

--- build/index.js
+++ build/index.js
@@ -23,33 +23,27 @@ const client = new discord_js_1.Client({
 const scrapeData = () => __awaiter(void 0, void 0, void 0, function* () {
     // Open browser instance
     const browser = yield puppeteer_1.default.launch({
-        headless: false,
-        defaultViewport: null,
+        headless: true,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
     });
     const url = 'https://wh40k.lexicanum.com/wiki/Special:Random';
     // Navigate to URL
     const page = yield browser.newPage();
     yield page.goto(url);
     console.log(`Navigating to ${url}`);
-    // await page.waitForSelector('#n-randompage a[href="/wiki/Special:Random"]');
-    // await page.click('#n-randompage a[href="/wiki/Special:Random"]');
-    // await page.waitForNavigation();
     try {
         // Scraping data
         const body = yield page.evaluate(() => {
             var _a;
             return (_a = document.querySelector('#bodyContent #mw-content-text p')) === null || _a === void 0 ? void 0 : _a.textContent;
         });
-        // const header = await page.evaluate(() => {
-        //     return document.querySelector('h1[class="firstHeading"]')?.textContent;
-        // });
         return body;
     }
     catch (err) {
         console.log(err);
     }
     finally {
-        yield page.setCacheEnabled(false);
         yield page.close();
         yield browser.close();
     }
@@ -81,7 +75,7 @@ client.on('ready', () => __awaiter(void 0, void 0, void 0, function* () {
     setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
         yield deleteEmbedMessage();
         yield sendEmbedMessage();
-    }), 60000);
+    }), 10000);
 }));
 // Logging the bot in to the server with token
 client.login(process.env.BOT_TOKEN);
