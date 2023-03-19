777c8cf19d211bc81685149d50488be60a2a412f
Nick <nicholasjperry1@gmail.com>
2023-03-19 01:41:44 -0400

    Changed npm start command to run ts-node; consistency in tsconfig to use project root '.'

--- build/index.js
+++ build/index.js
@@ -1,42 +1,29 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = require("discord.js");
-const dotenv_1 = __importDefault(require("dotenv"));
-const puppeteer_1 = __importDefault(require("puppeteer"));
-dotenv_1.default.config();
+const { EmbedBuilder, Client, GatewayIntentBits } = require('discord.js');
+const puppeteer = require('puppeteer');
+const dotenv = require('dotenv');
+const cron = require('node-cron');
+dotenv.config();
 // Instantiating bot/intents
-const client = new discord_js_1.Client({
-    intents: [discord_js_1.GatewayIntentBits.Guilds, discord_js_1.GatewayIntentBits.GuildMessages, discord_js_1.GatewayIntentBits.MessageContent, discord_js_1.GatewayIntentBits.GuildMembers]
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers]
 });
-const scrapeData = () => __awaiter(void 0, void 0, void 0, function* () {
+const scrapeData = async () => {
     // Open browser instance
-    const browser = yield puppeteer_1.default.launch({
+    const browser = await puppeteer.launch({
         headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        args: ["--no-sandbox", "--disable-setuid-sandbox", "--no-zygote"],
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
     });
     const url = 'https://wh40k.lexicanum.com/wiki/Special:Random';
     // Navigate to URL
-    const page = yield browser.newPage();
-    yield page.goto(url);
+    const page = await browser.newPage();
+    await page.goto(url);
     console.log(`Navigating to ${url}`);
     try {
         // Scraping data
-        const body = yield page.evaluate(() => {
-            var _a;
-            return (_a = document.querySelector('#bodyContent #mw-content-text p')) === null || _a === void 0 ? void 0 : _a.textContent;
+        const body = await page.evaluate(() => {
+            return document.querySelector('#bodyContent #mw-content-text p')?.textContent;
         });
         return body;
     }
@@ -44,38 +31,41 @@ const scrapeData = () => __awaiter(void 0, void 0, void 0, function* () {
         console.log(err);
     }
     finally {
-        yield page.close();
-        yield browser.close();
+        await page.close();
+        await browser.close();
     }
-});
+};
 // Sending embed message
-const sendEmbedMessage = () => __awaiter(void 0, void 0, void 0, function* () {
-    const data = yield scrapeData();
+const sendEmbedMessage = async () => {
+    const data = await scrapeData();
     const channel = client.channels.cache.get('1084926092600688740');
     channel.send({
         embeds: [
-            new discord_js_1.EmbedBuilder()
+            new EmbedBuilder()
                 .setDescription(data)
                 .setColor('Aqua')
         ]
     });
-});
-const deleteEmbedMessage = () => __awaiter(void 0, void 0, void 0, function* () {
+};
+const deleteEmbedMessage = async () => {
     const channel = client.channels.cache.get('1084926092600688740');
-    const messages = yield channel.messages.fetch();
-    const botMessage = messages.find((m) => { var _a; return m.author.id === ((_a = client === null || client === void 0 ? void 0 : client.user) === null || _a === void 0 ? void 0 : _a.id); });
+    const messages = await (channel).messages.fetch();
+    const botMessage = messages?.find((message) => message.author.id === client?.user?.id);
     if (botMessage) {
-        yield botMessage.delete();
+        await botMessage.delete();
     }
     else {
         return;
     }
-});
-client.on('ready', () => __awaiter(void 0, void 0, void 0, function* () {
-    setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
-        yield deleteEmbedMessage();
-        yield sendEmbedMessage();
-    }), 10000);
-}));
+};
 // Logging the bot in to the server with token
 client.login(process.env.BOT_TOKEN);
+// Ready event triggered
+client.on('ready', async () => {
+    cron.schedule('0 * * * *', async () => {
+        await deleteEmbedMessage();
+        await sendEmbedMessage();
+    }, {
+        timezone: 'America/New_York'
+    });
+});
