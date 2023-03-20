5999b2011f34450cd435cc4182c3054d53411b0c
Nick <nicholasjperry1@gmail.com>
2023-03-20 19:10:46 -0400

    package.json changes

--- build/index.js
+++ build/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const { EmbedBuilder, Client, GatewayIntentBits } = require('discord.js');
 const puppeteer = require('puppeteer');
 const dotenv = require('dotenv');
@@ -8,22 +17,23 @@ dotenv.config();
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers]
 });
-const scrapeData = async () => {
+const scrapeData = () => __awaiter(void 0, void 0, void 0, function* () {
     // Open browser instance
-    const browser = await puppeteer.launch({
+    const browser = yield puppeteer.launch({
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox", "--no-zygote"],
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
     });
     const url = 'https://wh40k.lexicanum.com/wiki/Special:Random';
     // Navigate to URL
-    const page = await browser.newPage();
-    await page.goto(url);
+    const page = yield browser.newPage();
+    yield page.goto(url);
     console.log(`Navigating to ${url}`);
     try {
         // Scraping data
-        const body = await page.evaluate(() => {
-            return document.querySelector('#bodyContent #mw-content-text p')?.textContent;
+        const body = yield page.evaluate(() => {
+            var _a;
+            return (_a = document.querySelector('#bodyContent #mw-content-text p')) === null || _a === void 0 ? void 0 : _a.textContent;
         });
         return body;
     }
@@ -31,13 +41,13 @@ const scrapeData = async () => {
         console.log(err);
     }
     finally {
-        await page.close();
-        await browser.close();
+        yield page.close();
+        yield browser.close();
     }
-};
+});
 // Sending embed message
-const sendEmbedMessage = async () => {
-    const data = await scrapeData();
+const sendEmbedMessage = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield scrapeData();
     const channel = client.channels.cache.get('1084926092600688740');
     channel.send({
         embeds: [
@@ -46,26 +56,26 @@ const sendEmbedMessage = async () => {
                 .setColor('Aqua')
         ]
     });
-};
-const deleteEmbedMessage = async () => {
+});
+const deleteEmbedMessage = () => __awaiter(void 0, void 0, void 0, function* () {
     const channel = client.channels.cache.get('1084926092600688740');
-    const messages = await (channel).messages.fetch();
-    const botMessage = messages?.find((message) => message.author.id === client?.user?.id);
+    const messages = yield (channel).messages.fetch();
+    const botMessage = messages === null || messages === void 0 ? void 0 : messages.find((message) => { var _a; return message.author.id === ((_a = client === null || client === void 0 ? void 0 : client.user) === null || _a === void 0 ? void 0 : _a.id); });
     if (botMessage) {
-        await botMessage.delete();
+        yield botMessage.delete();
     }
     else {
         return;
     }
-};
+});
 // Logging the bot in to the server with token
 client.login(process.env.BOT_TOKEN);
 // Ready event triggered
-client.on('ready', async () => {
-    cron.schedule('0 * * * *', async () => {
-        await deleteEmbedMessage();
-        await sendEmbedMessage();
-    }, {
+client.on('ready', () => __awaiter(void 0, void 0, void 0, function* () {
+    cron.schedule('0 * * * *', () => __awaiter(void 0, void 0, void 0, function* () {
+        yield deleteEmbedMessage();
+        yield sendEmbedMessage();
+    }), {
         timezone: 'America/New_York'
     });
-});
+}));
